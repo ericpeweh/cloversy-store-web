67fa129ed9e652e1f4bcdd4a6251009d8f7594a7
Eric Prima Wijaya <ericpeweh@gmail.com>
2022-06-30 16:06:38 +0700

    project initialization
    - Installed Next.js
    - Added npm packages (redux, redux toolkit, axios, material ui, formik)
    - Initialized custom material ui theme
    - created _document to manage font links, etc
    - added viewport metadata to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// Dependencies
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+
+// Styles
+import "../styles/globals.css";
+
+// Theme
+import mainTheme from "../styles/mainTheme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="initial-scale=1, width=device-width" />
+			</Head>
+			<ThemeProvider theme={mainTheme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
