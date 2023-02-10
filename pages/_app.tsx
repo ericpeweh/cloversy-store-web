e899d64d66511ddc4782a6fc1c573c9805df393b
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-02-10 21:19:58 +0700

    google analytic implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Provider as ReduxProvider } from "react-redux";
 import reduxStore from "../store";
 import Head from "next/head";
 import { Auth0Provider, User } from "@auth0/auth0-react";
+import ReactGA from "react-ga4";
 
 // Styles
 import "../styles/globals.css";
@@ -49,6 +50,25 @@ const App = ({ Component, pageProps }: AppProps) => {
 		observer.observe(body, { attributes: true, attributeFilter: ["style"] });
 	}, []);
 
+	// Google analytics setup
+	useEffect(() => {
+		const routeChangeHandler = (url: string) => {
+			// Initialize GA
+			ReactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID!);
+
+			// Track pageview
+			ReactGA.send({ hitType: "pageview", page: url });
+		};
+
+		router.events.on("routeChangeComplete", routeChangeHandler);
+
+		// Unsubscribe if component is unmounted
+		return () => {
+			router.events.off("routeChangeComplete", routeChangeHandler);
+		};
+	}, [router.events]);
+
+	// Redirect after auth if appState is provided
 	const redirectCallbackHandler = (
 		appState?: AppState | undefined,
 		_user?: User | undefined
