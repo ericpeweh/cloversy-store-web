eb63193907a4a0a64472fc9f2710549c30f8c76d
Eric Prima Wijaya <ericpeweh@gmail.com>
2023-05-02 21:18:46 +0700

    faq page update, not final

--- parts/Faq/Faq.tsx
+++ parts/Faq/Faq.tsx
@@ -12,53 +12,43 @@ const Faq = () => {
 			<PageTitle>Pertanyaan Umum</PageTitle>
 			<ContentContainer>
 				<FaqSubtitle>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem tempora ullam
-					dolores corrupti officiis, totam minus.
+					Berikut adalah beberapa pertanyaan yang sering diajukan kepada kami. Apabila jawaban kami
+					belum cukup menjawab pertanyaan, mohon hubungi admin.
 				</FaqSubtitle>
 				<Accordion
-					title="Lorem ipsu?"
-					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea."
+					title="Custom produk apa saja?"
+					description="Cloversy.id saat ini hanya menerima produk custom dengan media berupa: sepatu (kulit, kanvas, dll), tas & dompet (kulit), produk lain berbahan kulit."
 					testIds={{ summary: "faq-1" }}
 				/>
 				<Accordion
-					title="Ding elit. Corporis earum illum "
-					description="Ffugiat. Saepe mollitia quisquam quis repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad at soluta corporis odit inventore, dignissimos ea."
+					title="Apakah hasil custom tahan lama?"
+					description="Untuk masalah durabilitas produk sudah kita jamin, baik proses custom, penggunaan cat, treatment dan aditif. Kita secara kompeten mengupayakan produk dengan tingkat durabilitas tinggi."
 					testIds={{ summary: "faq-2" }}
 				/>
 				<Accordion
-					title="Crporis earum illum "
-					description="Uam quis repellendus at soluta corporis odit inventore, dignissimos ea. Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisq"
+					title="Apakah aman terkena air?"
+					description="Cat tidak akan luntur walaupun kena air, sama halnya saat mencuci."
+					testIds={{ summary: "faq-2" }}
+				/>
+				<Accordion
+					title="Cat apa yang dipakai?"
+					description="Cat yang kita gunakan full dari Angelus Paint, cat ini dibuat khusus untuk keperluan custom painting terutama bahan- bahan seperti kulit, canvas, dan lainnya."
 					testIds={{ summary: "faq-3" }}
 				/>
 				<Accordion
-					title="Dcing elit. Corporis earum illum ab"
-					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea. arum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea."
+					title="Berapa kisaran biaya custom painting?"
+					description="Biaya custom bergantung pada jenis bahan dan kesulitan design, berkisar dari 150K untuk yang paling sederhana hingga jutaan rupiah."
 					testIds={{ summary: "faq-4" }}
 				/>
 				<Accordion
-					title="Elit Corporis earum illum a"
-					description="Piu psum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fug"
+					title="Apakah bisa menggunakan design sendiri?"
+					description="Tentu saja bisa, konsep dan design dapat disediakan oleh customer, atau customer dapat memilih design yang telah disediakan pada katalog produk Cloversy.id"
 					testIds={{ summary: "faq-5" }}
 				/>
 				<Accordion
-					title="Dcing elit. Corporis earum illum ab"
-					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea. arum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea."
+					title="Bagaimana cara merawat produk custom?"
+					description="Merawat produk custom sebenarnya mirip dengan merawat produk utuh pada umumnya. Sebisa mungkin menyimpan pada tempat lembab, dan tidak menggosok terlalu kuat saat mencuci, gunakan sikat yang tidak kasar dan gunakan sabun pencuci khusus."
 					testIds={{ summary: "faq-6" }}
-				/>
-				<Accordion
-					title="Lorem ipsu?"
-					description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fugiat. Saepe mollitia quisquam quis repellendus at soluta corporis odit inventore, dignissimos ea."
-					testIds={{ summary: "faq-7" }}
-				/>
-				<Accordion
-					title="Ding elit. Corporis earum illum "
-					description="Ffugiat. Saepe mollitia quisquam quis repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad at soluta corporis odit inventore, dignissimos ea."
-					testIds={{ summary: "faq-8" }}
-				/>
-				<Accordion
-					title="Elit Corporis earum illum a"
-					description="Piu psum dolor sit amet consectetur adipisicing elit. Corporis earum illum ab modi officia, pariatur quo ad fug"
-					testIds={{ summary: "faq-9" }}
 				/>
 			</ContentContainer>
 		</FaqContainer>
